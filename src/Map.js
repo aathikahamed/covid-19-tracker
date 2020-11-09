61fd3e198f6c1d1adbda210a3ef3b2fde54d667b
Aathik Ahamed <aathikahamed@gmail.com>
2020-11-09 11:16:47 +0000

    changed http to https

--- src/Map.js
+++ src/Map.js
@@ -9,7 +9,7 @@ function Map({ countries, casesType, center, zoom }) {
       <LeafletMap center={center} zoom={zoom}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright"></a> contributions'
+          attribution='&copy; <a href="https://osm.org/copyright"></a> contributions'
         />
         {showDataOnMap(countries, casesType)}
       </LeafletMap>
