f280e9e9d38d698ce013a5c16275e46004e25fba
Aathik Ahamed <aathikahamed@gmail.com>
2020-11-14 13:00:58 +0000

    added search functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import {
-  MenuItem,
-  FormControl,
-  Select,
-  Card,
-  CardContent,
-} from "@material-ui/core";
+import { FormControl, Card, CardContent } from "@material-ui/core";
 import InfoBox from "./InfoBox";
 import Map from "./Map";
 import Table from "./Table";
@@ -16,10 +10,11 @@ import "leaflet/dist/leaflet.css";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import firebase from "firebase/app";
 import "firebase/analytics";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 
 function App() {
   const [countries, setCountries] = useState([]);
-  const [country, setCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState({});
   const [tableData, setTableData] = useState([]);
   const [mapCenter, setMapCenter] = useState({ lat: 20.80746, lng: 20.4796 });
@@ -28,11 +23,7 @@ function App() {
   const [casesType, setCasesType] = useState("cases");
 
   useEffect(() => {
-    fetch("https://disease.sh/v3/covid-19/all")
-      .then((response) => response.json())
-      .then((data) => {
-        setCountryInfo(data);
-      });
+    fetchWorldWideData();
   }, []);
 
   useEffect(() => {
@@ -58,20 +49,26 @@ function App() {
   }, []);
 
   const onCountryChange = (event) => {
-    const countryCode = event.target.value;
-
-    const url =
-      countryCode === "worldwide"
-        ? "https://disease.sh/v3/covid-19/all"
-        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
-
+    const countryCode = event.target.innerHTML;
+    const url = `https://disease.sh/v3/covid-19/countries/${countryCode}`;
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        setCountry(countryCode);
+        try {
+          setCountryInfo(data);
+          setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+          setMapZoom(4);
+        } catch (error) {
+          fetchWorldWideData();
+        }
+      });
+  };
+  const fetchWorldWideData = () => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => {
         setCountryInfo(data);
-        setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
-        setMapZoom(4);
+        setMapCenter({ lat: 20.80746, lng: 20.4796 });
       });
   };
 
@@ -81,8 +78,7 @@ function App() {
         .then((response) => response.json())
         .then((data) => {
           const countries = data.map((country) => ({
-            name: country.country, // United Kingdom
-            value: country.countryInfo.iso2, //UK
+            name: country.country,
           }));
           const sortedData = sortData(data);
           setTableData(sortedData);
@@ -98,19 +94,19 @@ function App() {
         <div className="app__header">
           <h1>COVID-19 TRACKER</h1>
           <FormControl className="app__dropdown">
-            <Select
-              varient="outlined"
+            <Autocomplete
               onChange={onCountryChange}
-              value={country}
-            >
-              <MenuItem value="worldwide">Worldwide</MenuItem>
-
-              {countries.map((country, index) => (
-                <MenuItem key={index} value={country.value}>
-                  {country.name}
-                </MenuItem>
-              ))}
-            </Select>
+              options={countries}
+              getOptionLabel={(country) => country.name}
+              style={{ width: 300 }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Search Country"
+                  variant="outlined"
+                />
+              )}
+            />
           </FormControl>
         </div>
 
