787a4955222dcac6ff3e839b18ef1bd6b6d341b5
Aathik Ahamed <aathikahamed@gmail.com>
2020-11-18 19:29:57 +0000

    made some changes

--- public/worker.js
+++ public/worker.js
@@ -1,42 +1,39 @@
-var CACHE_NAME = "pwa-task-manager";
-var urlsToCache = ["/", "/completed"];
+const CACHE_NAME = "version-1";
+const urlsToCache = ["offline.html"];
 
-// Install a service worker
+const self = this;
+
 self.addEventListener("install", (event) => {
-  // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
+    caches.open(CACHE_NAME).then((cache) => {
       console.log("Opened cache");
+
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-// Cache and return requests
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(function (response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
     })
   );
 });
 
-// Update a service worker
 self.addEventListener("activate", (event) => {
-  var cacheWhitelist = ["pwa-task-manager"];
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
         cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+          if (!cacheWhitelist.includes(cacheName)) {
             return caches.delete(cacheName);
           }
         })
-      );
-    })
+      )
+    )
   );
 });
